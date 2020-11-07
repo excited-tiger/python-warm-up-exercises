75112a847e004164090cfaff02644f41e8a555e0
孙振平 <sunzhenping@starsee.cn>
2020-11-07 21:54:57 +0800

    feat(algorithm): add soloution2 poj 2811

--- interview/poj2811_enumerate.py
+++ interview/poj2811_enumerate.py
@@ -3,7 +3,6 @@
 """
 poj 2811 熄灯问题 枚举（enumerate）
 """
-import sys
 from copy import deepcopy
 
 
@@ -15,14 +14,10 @@ def main():
 
     # 第一行开关确定下以后，后面的结果就确定了
     # 第一行开关的组合有2^6种
-    total_combination_ls = []
     for i in range(2**(len(light_matrix[0]))):
         # 对应的组合 刚好是二进制数
         # {:0{}b} {:08b} 二进制数 用0填充8位
         str_i = '{:0{}b}'.format(i, len(light_matrix[0]))
-
-        total_combination_ls.append(str_i)
-
         press_matrix = [['0' for i in range(len(light_matrix[0]))]
                         for j in range(len(light_matrix))]
         temp_light_matrix = deepcopy(light_matrix)
@@ -30,14 +25,15 @@ def main():
         for x_id, x in enumerate(str_i):
             if x == '1':
                 press_matrix[0][x_id] = '1'
-                temp_light_matrix = close_light(temp_light_matrix, [0, x_id])
+                temp_light_matrix = turn_off_light(temp_light_matrix,
+                                                   [0, x_id])
         # 判断是否所有的灯都熄灭了 并返回按压过的结果
         bool_res, press_matrix = is_closed_all(temp_light_matrix, press_matrix)
 
+        # 如果灯都灭了 返回按压结果
         if bool_res:
             press_str = '\n'.join([' '.join(i) for i in press_matrix])
-            # print(press_str)
-            sys.stdout.write(press_str)
+            print(press_str)
             return press_str
 
 
@@ -48,14 +44,15 @@ def is_closed_all(light_matrix: list, press_matrix: list) -> tuple:
         for x_id, x in enumerate(light_matrix[y - 1]):
             if x == '1':
                 press_matrix[y][x_id] = '1'
-                light_matrix = close_light(light_matrix, [y, x_id])
+                light_matrix = turn_off_light(light_matrix, [y, x_id])
+    # 判断最后一行灯的状态
     for light in light_matrix[y]:
         if light == '1':
             return False, []
     return True, press_matrix
 
 
-def close_light(light_matrix: list, light_pos: list) -> list:
+def turn_off_light(light_matrix: list, light_pos: list) -> list:
     min_x, max_x, min_y, max_y = 0, len(
         light_matrix[0]) - 1, 0, len(light_matrix) - 1
     y, x = light_pos
@@ -69,6 +66,67 @@ def close_light(light_matrix: list, light_pos: list) -> list:
     return light_matrix
 
 
+# 解法二
+class TurnOffLight(object):
+    def __init__(self):
+        self.light_matrix = []
+        for i in range(5):
+            input_str = input('请输入灯的状态\n')
+            self.light_matrix.append(
+                [int(i) for i in input_str.strip().split(' ') if i])
+
+        self.press_matrix = [[0 for i in range(len(self.light_matrix[0]))]
+                             for j in range(len(self.light_matrix))]
+
+    @staticmethod
+    def get_val(mat, row, col):
+        min_x, max_x, min_y, max_y = 0, len(mat[0]) - 1, 0, len(mat) - 1
+        if col < min_x or col > max_x or row < min_y or row > max_y:
+            return 0
+        else:
+            return mat[row][col]
+
+    def check_all_light(self):
+        press = self.press_matrix
+        for r in range(len(self.light_matrix) - 1):
+            for c in range(len(self.light_matrix[0])):
+
+                press[r + 1][c] = (self.get_val(self.light_matrix, r, c) +
+                                   self.get_val(press, r, c) +
+                                   self.get_val(press, r - 1, c) +
+                                   self.get_val(press, r, c - 1) +
+                                   self.get_val(press, r, c + 1)) % 2
+        r = len(self.light_matrix) - 1
+        # 判断最后是否可以把灯关掉
+        for c in range(len(self.light_matrix[0])):
+            if press[r][c] != (self.get_val(self.light_matrix, r, c) +
+                               self.get_val(press, r - 1, c) +
+                               self.get_val(press, r, c - 1) +
+                               self.get_val(press, r, c + 1)) % 2:
+                return False
+        return True
+
+    def turn_off_light(self):
+
+        while not self.check_all_light():
+            self.press_matrix[0][0] += 1
+            c = 0
+            while self.press_matrix[0][c] > 1:
+                self.press_matrix[0][c] = 0
+                c += 1
+                self.press_matrix[0][c] += 1
+            continue
+        res = ''
+        for l in self.press_matrix:
+            line = ''
+            for i in l:
+                line += str(i) + ' '
+            res += line.strip() + '\n'
+
+        print(res)
+        return res
+
+
 if __name__ == '__main__':
     t = """
 0 1 1 0 1 0
@@ -77,4 +135,6 @@ if __name__ == '__main__':
 1 0 0 1 0 1
 0 1 1 1 0 0"""
 
-    main()
+    # main()
+    tf = TurnOffLight()
+    tf.turn_off_light()
