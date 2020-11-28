68ebb41bd23ef9bdc3ca28e5975ddd2b37a6f877
孙振平 <sunzhenping@starsee.cn>
2020-11-28 23:02:07 +0800

    feat(data structure): add double_linked_list.py

---
+++ data_structure/double_linked_list.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+# @atime    : 2020/11/28 10:07 下午
+
+"""
+double linked list
+"""
+
+
+class Node:
+    def __init__(self):
+        self.data = None
+        self.pre = None
+        self.next = None
+
+    def __str__(self):
+        return f'{self.data}'
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def delete(self, data):
+        pass
+
+    def delete_nth(self, index):
+        pass
+
+    def delete_head(self):
+        pass
+
+    def delete_tail(self):
+        pass
+
+    def insert_nth(self):
+        pass
+
+    def insert_head(self):
+        pass
+
+    def insert_tail(self):
+        pass
+
+    def is_empty(self):
+        pass
