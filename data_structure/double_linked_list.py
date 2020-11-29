0a3d42b23e6dd64796e4a119661c29c0dbc41078
孙振平 <sunzhenping@starsee.cn>
2020-11-30 00:18:36 +0800

    feat(data structure): complete double_linked_list

--- data_structure/double_linked_list.py
+++ data_structure/double_linked_list.py
@@ -7,8 +7,8 @@ double linked list
 
 
 class Node:
-    def __init__(self):
-        self.data = None
+    def __init__(self, data):
+        self.data = data
         self.pre = None
         self.next = None
 
@@ -18,37 +18,145 @@ class Node:
 
 class DoubleLinkedList:
     def __init__(self):
-        pass
+        self.head = None
+        self.tail = None
+        self.length = 0
 
     def __iter__(self):
-        pass
+        n = self.head
+        while n:
+            yield str(n.data)
+            n = n.next
 
     def __len__(self):
-        pass
+        return self.length
 
     def __str__(self):
-        pass
+        return '->'.join(self)
 
     def delete(self, data):
-        pass
+        n = self.head
+        while n:
+            if n.data == data:
+                break
+            n = n.next
+        if not n or n.data != data:
+            raise IndexError
+        self.length -= 1
+        if not n.pre:
+            return self.delete_head()
+        elif not n.next:
+            return self.delete_tail()
+        else:
+            pre = n.pre
+            pre.next = n.next
+            n.next.pre = pre
+            return n
 
     def delete_nth(self, index):
-        pass
+        if not 0 <= index < self.length:
+            raise IndexError
+        if index == 0:
+            if not self.head:
+                raise IndexError
+            del_node = self.head
+            self.head = self.head.next
+            self.head.pre = None
+        else:
+            cur = self.head
+            for _ in range(index):
+                cur = cur.next
+            del_node = cur
+            if index == self.length - 1:
+                self.tail = cur.pre
+                cur.pre.next = None
+            else:
+                pre = cur.pre
+                pre.next = cur.next
+                cur.next.pre = pre
+
+        del_node.next = None
+        del_node.pre = None
+        self.length -= 1
+        return del_node.data
 
     def delete_head(self):
-        pass
+        return self.delete_nth(0)
 
     def delete_tail(self):
-        pass
+        return self.delete_nth(self.length - 1)
 
-    def insert_nth(self):
-        pass
+    def insert_nth(self, index, data):
+        if not 0 <= index <= self.length:
+            raise IndexError
+        if index == 0:
+            n = Node(data)
+            if not self.head:
+                self.head = n  # raise IndexError
+            else:
+                n.next = self.head
+                self.head.pre = n
+                self.head = n
+        else:
+            cur = self.head
+            for _ in range(index - 1):
+                cur = cur.next
+            n = Node(data)
+            if not cur.next:
+                self.tail = n
+            n.next = cur.next
+            n.pre = cur
+            cur.next = n
+        self.length += 1
 
-    def insert_head(self):
-        pass
+    def insert_head(self, data):
+        self.insert_nth(0, data)
 
-    def insert_tail(self):
-        pass
+    def insert_tail(self, data):
+        self.insert_nth(self.length, data)
 
     def is_empty(self):
-        pass
+        return not bool(self.length)
+
+
+def test_doubly_linked_list() -> None:
+    """
+    >>> test_doubly_linked_list()
+    """
+    linked_list = DoubleLinkedList()
+    assert linked_list.is_empty() is True
+    assert str(linked_list) == ""
+
+    try:
+        linked_list.delete_head()
+        assert False  # This should not happen.
+    except IndexError:
+        assert True  # This should happen.
+
+    try:
+        linked_list.delete_tail()
+        assert False  # This should not happen.
+    except IndexError:
+        assert True  # This should happen.
+
+    for i in range(10):
+        assert len(linked_list) == i
+        linked_list.insert_nth(i, i + 1)
+    assert str(linked_list) == "->".join(str(i) for i in range(1, 11))
+
+    linked_list.insert_head(0)
+    linked_list.insert_tail(11)
+    assert str(linked_list) == "->".join(str(i) for i in range(0, 12))
+    assert linked_list.delete_head() == 0
+
+    assert linked_list.delete_nth(9) == 10
+
+    assert linked_list.delete_tail() == 11
+    assert len(linked_list) == 9
+    assert str(linked_list) == "->".join(str(i) for i in range(1, 10))
+
+
+if __name__ == "__main__":
+    # from doctest import testmod
+    # testmod()
+    test_doubly_linked_list()
