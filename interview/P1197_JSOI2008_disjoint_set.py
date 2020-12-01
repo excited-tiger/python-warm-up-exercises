fafd73b7f36f714005302cb515308df16ba7aa49
孙振平 <sunzhenping@starsee.cn>
2020-12-01 23:15:54 +0800

    feat(interview): 并查集-P1197_JSOI2008_星球大战
    解法2

--- interview/P1197_JSOI2008_disjoint_set.py
+++ interview/P1197_JSOI2008_disjoint_set.py
@@ -8,49 +8,6 @@ https://www.luogu.com.cn/problem/P1197
 from typing import List
 
 
-def main(input_str: str) -> str:
-    """
-    Args:
-        input_str (str):输入的字符串数据
-    Returns: output str 连通块个数
-    """
-    union_list = []
-    destroy_list = []
-    # 1. 解析数据
-    input_str_list = input_str.split('\n')
-    planet_num, union_num, destroy_num = 0, 0, 0
-
-    for line in input_str_list:
-        line = line.strip()
-        if not line:
-            continue
-        line_list = line.split(' ')
-        if len(line_list) > 1:
-            if not planet_num and not union_num:
-                planet_num, union_num = int(line_list[0]), int(line_list[1])
-            else:
-                union_list.append((int(line_list[0]), int(line_list[1])))
-        else:
-            if not destroy_num:
-                destroy_num = int(line_list[0])
-            else:
-                destroy_list.append(int(line_list[0]))
-
-    # 2. 连通块个数
-    members = list(range(planet_num))
-    # 初始状态连通块个数
-    origin_set_num, union_list = get_union_num(union_list, members=members)
-    final_res = [str(origin_set_num)]
-    # 获取每次打击后的连通块个数
-    for idx, i in enumerate(destroy_list):
-        members.remove(i)
-        union_num, union_list = get_union_num(union_list, members=members,
-                                              destroy=i)
-        final_res.append(str(union_num))
-
-    return '\n'.join(final_res)
-
-
 class Disjoint():
     def __init__(self, data):
         self.data = data
@@ -91,6 +48,65 @@ def merge(x: Disjoint, y: Disjoint) -> tuple:
     return x, y
 
 
+def parse_data(input_str: str) -> tuple:
+    """
+    解析输入数据
+    Args:
+        input_str (str):输入的字符串
+
+    Returns: tuple
+        行星个数 int
+        行星可以连接的所有组合 list
+        要摧毁的所有行星 list
+    """
+    union_list = []
+    destroy_list = []
+
+    input_str_list = input_str.split('\n')
+    planet_num, union_num, destroy_num = 0, 0, 0
+
+    for line in input_str_list:
+        line = line.strip()
+        if not line:
+            continue
+        line_list = line.split(' ')
+        if len(line_list) > 1:
+            if not planet_num and not union_num:
+                planet_num, union_num = int(line_list[0]), int(line_list[1])
+            else:
+                union_list.append((int(line_list[0]), int(line_list[1])))
+        else:
+            if not destroy_num:
+                destroy_num = int(line_list[0])
+            else:
+                destroy_list.append(int(line_list[0]))
+    return planet_num, union_list, destroy_list
+
+
+def main(input_str: str) -> str:
+    """
+    Args:
+        input_str (str):输入的字符串数据
+    Returns: output str 连通块个数
+    """
+    # 1. 解析数据
+    planet_num, union_list, destroy_list = parse_data(input_str)
+
+    # 2. 连通块个数
+    members = list(range(planet_num))
+    # 初始状态连通块个数
+    origin_set_num, union_list = get_union_num(union_list, members=members)
+    final_res = [str(origin_set_num)]
+    # 获取每次打击后的连通块个数
+    for idx, i in enumerate(destroy_list):
+        members.remove(i)
+        union_num, union_list = get_union_num(union_list, members=members,
+                                              destroy=i)
+        final_res.append(str(union_num))
+
+    return '\n'.join(final_res)
+
+
 def get_union_num(union_list: List[tuple], members: list,
                   destroy=None) -> tuple:
     """
@@ -116,6 +132,37 @@ def get_union_num(union_list: List[tuple], members: list,
         if m not in parent_list:
             parent_list.append(m)
     return len(parent_list), new_union_list
+
+
+def main2(input_str):
+    # 1. 解析数据
+    planet_num, union_list, destroy_list = parse_data(input_str)
+    final_res = []
+    total_members_map = {i: Disjoint(i) for i in range(planet_num)}
+
+    iter_nums = len(destroy_list) + 1
+    for _ in range(iter_nums):
+        next_iter_union_list = []
+        temp_union_list = []
+        for x, y in union_list:
+            if x in destroy_list or y in destroy_list:
+                next_iter_union_list.append((x, y))
+                continue
+            total_members_map[x], total_members_map[y] = merge(
+                total_members_map[x], total_members_map[y])
+
+        for i in total_members_map:
+            if i not in destroy_list:
+                i_p = find(total_members_map[i])
+                if i_p.data not in temp_union_list:
+                    temp_union_list.append(i_p.data)
+
+        final_res.append(str(len(temp_union_list)))
+        union_list = next_iter_union_list
+
+        if destroy_list:
+            destroy_list.pop()
+    return '\n'.join(final_res[::-1])
 
 
 if __name__ == '__main__':
@@ -148,4 +195,9 @@ if __name__ == '__main__':
         3. 遍历没有被攻击的行星，找其根节点，计算根节点的数目就是连通块个数
         4. 再遍历新的组合 计算连通块个数（会重复遍历）
     """
-    main(input_)
+    # main(input_)
+    """
+    解法二：逆向思维
+        1. 被摧毁最多-> 没有被摧毁，由最少组合不断扩充，减少遍历次数
+    """
+    main2(input_)
